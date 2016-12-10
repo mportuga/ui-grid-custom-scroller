7826fca355d0b160d6be332903251222d1fa5002
Portugal, Marcelo <Marcelo.Portugal@Kronos.com>
2016-12-09 22:09:18 -0500

    docs(ngdocs): Adding ngdocs to git so that it can be viewed.

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,56 @@
+NG_DOCS={
+  "sections": {
+    "api": "API"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "ui.grid.customScroller",
+      "shortName": "ui.grid.customScroller",
+      "type": "overview",
+      "moduleName": "ui.grid.customScroller",
+      "shortDescription": "#ui.grid.customScroller",
+      "keywords": "alternative api custom customscroller devices ensure event events grid lag module native overview scroll scroller scrolling touch ui works"
+    },
+    {
+      "section": "api",
+      "id": "ui.grid.customScroller.constant:uiGridScrollerConstants",
+      "shortName": "uiGridScrollerConstants",
+      "type": "object",
+      "moduleName": "ui.grid.customScroller",
+      "shortDescription": "Constants for use with the  uiGridScroller",
+      "keywords": "api calculates constant constants current currently customscroller deceleration event events grid momentum mouse mousedown mousemove mouseup object options pointer pointerdown pointermove pointerup progress scroll scrollevent scrolling scrolltype service set touchable touchend touchmove touchstart triggered type ui uigridscroller uigridscrollerconstants"
+    },
+    {
+      "section": "api",
+      "id": "ui.grid.customScroller.directive:uiGridCustomScroller",
+      "shortName": "uiGridCustomScroller",
+      "type": "directive",
+      "moduleName": "ui.grid.customScroller",
+      "shortDescription": "Updates the grid to use the gridScroller instead of the jquery scroll event",
+      "keywords": "$log $scope address age angular api app boston city class columndefs comics company controller customscroller data directive displayname div doe doppelgangers email enablepinning event floor friend friends function generatedata grid gridoptions gridscroller html jake jane joe john jquery js ma mainctrl math module newbury ng-controller ngtouch phone pinnedleft pinnedright pinning push random return scroll st street total ui ui-grid ui-grid-custom-scroller ui-grid-pinning updates var width zip"
+    },
+    {
+      "section": "api",
+      "id": "ui.grid.customScroller.service:uiGridScroller",
+      "shortName": "uiGridScroller",
+      "type": "service",
+      "moduleName": "ui.grid.customScroller",
+      "shortDescription": "uiGridScroller is an alternative to the native scroll event that uses touch events to ensure that grid scrolling works",
+      "keywords": "alternative amount animate animating animation api axis base based calcnewmove calcnewpos calculates callback called coordinate coordinates curr current customscroller delta destination destpos destx desty devices duration ease easeclb easeres element ensure event events finishes frames function getmaxscroll grid horizontal ideal initial initializes initiated isanimating lag limit limits long momentum move moved movement native object original pointer position positions relpoint resolution scroll scrolled scrollhandler scrolling scrollpos service sets start started startpos starts supposed target time times touch track translate triggered type ui uigridscroller ultimately updated updates vertical works"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": true,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.js",
+    "angular-animate.min.js",
+    "angular-touch.min.js",
+    "ui-grid.min.js",
+    "ui-grid-custom-scroller.min.js"
+  ]
+};
